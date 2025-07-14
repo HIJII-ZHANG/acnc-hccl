fe401bf8dbb2863647409ed1671130600cad03b0
yanyefeng <yanyefeng@huawei.com>
2025-07-14 11:24:29 +0000

    !68 dataType revert Merge pull request !68 from yanyefeng/master

--- inc/hccl/hccl_types.h
+++ inc/hccl/hccl_types.h
@@ -81,9 +81,6 @@ typedef enum {
     HCCL_DATA_TYPE_FP64 = 10,    /**< fp64 */
     HCCL_DATA_TYPE_BFP16 = 11,    /**< bfp16 */
     HCCL_DATA_TYPE_INT128 = 12,   /**< int128 */
-    HCCL_DATA_TYPE_HIF8 = 14,     /**< hif8 */
-    HCCL_DATA_TYPE_FP8E4M3 = 15,  /**< fp8e4m3 */
-    HCCL_DATA_TYPE_FP8E5M2 = 16,  /**< fp8e5m2 */
     HCCL_DATA_TYPE_RESERVED = 255 /**< reserved */
 } HcclDataType;
 
